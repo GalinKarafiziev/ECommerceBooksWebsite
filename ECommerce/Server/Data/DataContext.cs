5e0b7729d8a85dc37d3dc3ef60fd413bf3fb749e
galin <galin@DESKTOP-LS26IQD>
2022-03-30 17:16:02 +0300

    Add project files.

---
+++ ECommerce/Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace ECommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Attack on Titan, Vol. 1",
+                    Description = "Several hundred years ago, humans were almost exterminated by giants. A small percentage of humanity survived by barricading themselves in a city protected by walls even taller than the tallest giants. Now the city has not seen a giant for over 100 years, before Elen and Mikasa witness the walls destruction by a super-giant.",
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/91M9VaZWxOL.jpg",
+                    Price = 9.99m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Kimetsu no Yaiba, Vol. 1",
+                    Description = "Learning to destroy demons won’t be easy, and Tanjiro barely knows where to start. The surprise appearance of another boy named Giyu, who seems to know what’s going on, might provide some answers—but only if Tanjiro can stop Giyu from killing his sister first!",
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/81ZNkhqRvVL.jpg",
+                    Price = 9.99m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Jujutsu Kaisen, Vol. 1",
+                    Description = "To gain the power he needs to save his friend from a cursed spirit, Yuji Itadori swallows a piece of a demon, only to find himself caught in the midst of a horrific war of the supernatural!",
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51zXJBloNpL._SX331_BO1,204,203,200_.jpg",
+                    Price = 9.99m
+                }
+                );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ECommerce/Server/Migrations/20220330140439_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Several hundred years ago, humans were almost exterminated by giants. A small percentage of humanity survived by barricading themselves in a city protected by walls even taller than the tallest giants. Now the city has not seen a giant for over 100 years, before Elen and Mikasa witness the walls destruction by a super-giant.", "https://images-na.ssl-images-amazon.com/images/I/91M9VaZWxOL.jpg", 9.99m, "Attack on Titan, Vol. 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Learning to destroy demons won’t be easy, and Tanjiro barely knows where to start. The surprise appearance of another boy named Giyu, who seems to know what’s going on, might provide some answers—but only if Tanjiro can stop Giyu from killing his sister first!", "https://images-na.ssl-images-amazon.com/images/I/81ZNkhqRvVL.jpg", 9.99m, "Kimetsu no Yaiba, Vol. 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "To gain the power he needs to save his friend from a cursed spirit, Yuji Itadori swallows a piece of a demon, only to find himself caught in the midst of a horrific war of the supernatural!", "https://images-na.ssl-images-amazon.com/images/I/51zXJBloNpL._SX331_BO1,204,203,200_.jpg", 9.99m, "Jujutsu Kaisen, Vol. 1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
